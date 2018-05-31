36d08092b6f79b46f2bce828f23821e2d5e1373c
Roman <mantilocal@gmail.com>
2018-05-31 22:41:20 +0300

    comm for pull request, see look at commit below

--- src/dataBase/dbConnection/dataBase.go
+++ src/dataBase/dbConnection/dataBase.go
@@ -4,19 +4,19 @@ import (
 	"fmt"
 	"database/sql"
 	"log"
-	_"github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 //Specify this values for concrette db
 const (
-	DB_USER     = "postgres"
-	DB_PASSWORD = "root"
-	DB_NAME     = "postgres"
-	DB_SCHEMA   = "gotoboox."
-	DB_USERS_TABLE = "users"
-	DB_BOOKS_TABLE = "books"
-	DB_AUTHORS_TABLE = "authors"
-	DB_CATEGORIES_TABLE = "categories"
+	DB_USER                = "postgres"
+	DB_PASSWORD            = "root"
+	DB_NAME                = "postgres"
+	DB_SCHEMA              = "gotoboox."
+	DB_USERS_TABLE         = "users"
+	DB_BOOKS_TABLE         = "books"
+	DB_AUTHORS_TABLE       = "authors"
+	DB_CATEGORIES_TABLE    = "categories"
 	DB_BOOKS_AUTHORS_TABLE = "books_authors"
 )
 
@@ -25,7 +25,7 @@ var GlobalDataBaseConnection *sql.DB
 
 //InitializeConnection is a function that is used to open connection
 //with a dataBase.
-func InitializeConnection()  {
+func InitializeConnection() {
 	var err error
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
 	GlobalDataBaseConnection, err = sql.Open("postgres", dbinfo)
