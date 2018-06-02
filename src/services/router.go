24626e032e07ad1892fa2415eb40db1703109706
Roman <mantilocal@gmail.com>
2018-06-02 19:02:48 +0300

    structure, gin, database, repos

--- src/services/router.go
+++ src/services/router.go
@@ -5,14 +5,24 @@ import (
 	"log"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"github.com/metalscreame/GoToBoox/src/services/api/v1"
-	"github.com/metalscreame/GoToBoox/src/services/api/v1/userProfile"
+	"github.com/metalscreame/GoToBoox/src/services/authentification/midlware"
+	"github.com/metalscreame/GoToBoox/src/dataBase/repository/users"
+	"github.com/metalscreame/GoToBoox/src/dataBase"
 )
 
 const (
-	apiV1route       = "/api/v1"
-	userProfileRoute = "/userProfile"
+	apiRoute = "/api/v1"
 )
+
+type UserService struct {
+	UsersRepo users.UserRepository
+}
+
+func NewUserService(repository users.UserRepository) *UserService {
+	return &UserService{
+		UsersRepo:repository,
+	}
+}
 
 var router *gin.Engine
 
@@ -21,7 +31,7 @@ func InitializeRouter() {
 	port := os.Getenv("PORT")
 
 	//Uncomment for local machine   !!!!
-	//port="8080"
+	port="8080"
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
@@ -38,15 +48,59 @@ func InitializeRouter() {
 
 	//The place for handlers routes
 	// exm router.GET("/", showIndex.ShowIndexPage)
-	router.GET("/api/v.1/", v1.IndexHandler)
+	router.GET("/api/v.1/", IndexHandler)
 	initUserProfileRouters()
 
 	router.Run(":" + port)
 }
 
 func initUserProfileRouters() {
-	router.GET(apiV1route+userProfileRoute, userProfile.UserGet)
-	router.POST(apiV1route+userProfileRoute, userProfile.UserCreate)
-	router.PUT(apiV1route+userProfileRoute, userProfile.UserUpdate)
-	router.DELETE(apiV1route+userProfileRoute, userProfile.UserDelete)
+
+	// Use the SetUserStatus middleware for every route to set a flag
+	// indicating whether the request was from an authenticated user or not
+	router.Use(midlware.SetUserStatus())
+
+	service:=NewUserService(users.NewPostgresUsersRepo(dataBase.GlobalDataBaseConnection))
+
+	userRoutes := router.Group(apiRoute)
+	{
+		// Handle POST requests at /api/v1/login
+		// Ensure that the user is not logged in by using the middleware
+		userRoutes.POST("/login", midlware.EnsureNotLoggedIn(), service.PerformLoginHandler)
+
+		// Handle GET requests at /api/v1/logout
+		// Ensure that the user is logged in by using the middleware
+		userRoutes.GET("/logout",  midlware.EnsureLoggedIn(), service.LogoutHandler)
+
+		// Handle POST requests at /api/v1/register
+		// Ensure that the user is not logged in by using the middleware
+		userRoutes.POST("/register",  midlware.EnsureNotLoggedIn(), service.UserCreateHandler)
+
+		// Handle the GET requests at /api/v1/register
+		// Show the user's profile page
+		// Ensure that the user is logged in by using the middleware
+		userRoutes.GET("/userProfile", midlware.EnsureLoggedIn(), service.UserGetHandler)
+
+		// Handle the GET requests at /api/v1/register
+		// Show the user's profile page
+		// Ensure that the user is logged in by using the middleware
+		userRoutes.PUT("/userProfile",  midlware.EnsureLoggedIn(),service.UserUpdateHandler)
+
+		// Handle the GET requests at /api/v1/register
+		// Show the user's profile page
+		// Ensure that the user is logged in by using the middleware
+		userRoutes.DELETE("/userProfile",  midlware.EnsureLoggedIn(),service.UserDeleteHandler)
+	}
+
+	// Show the login page
+	// Ensure that the user is not logged in by using the middleware
+	router.GET("/login", midlware.EnsureNotLoggedIn(), ShowLoginPage)
+
+	// Show the registration page
+	// Ensure that the user is not logged in by using the middleware
+	router.GET("/register",  midlware.EnsureNotLoggedIn(), ShowRegistrPage)
+
+	// Show the user's profile page
+	// Ensure that the user is logged in by using the middleware
+	router.GET("/usersProfile", midlware.EnsureLoggedIn(),ShowUsersProfilePage)
 }
