96f1b4dc7bcf389e4b92fd5f512e2ea2dab0ad6c
Den <away4people@gmail.com>
2018-06-06 01:11:12 +0300

    Update index.go

--- src/services/index.go
+++ src/services/index.go
@@ -2,21 +2,19 @@ package services
 
 import (
 	"github.com/gin-gonic/gin"
-	//"github.com/metalscreame/GoToBoox/src/models"
 	"net/http"
 	"github.com/metalscreame/GoToBoox/src/dataBase/postgres"
-	//"github.com/metalscreame/GoToBoox/src/dataBase"
 	"github.com/metalscreame/GoToBoox/src/dataBase/repository"
 )
 
 func IndexHandler(c *gin.Context) {
 	type Data struct{
-//		PopularBooks []repository.Book
+		PopularBooks []repository.Book
 		Categories []repository.Categories
 	}
 
-//	bookRepo := postgres.NewBooksRepository(dataBase.Connection)
-//	books, _ := bookRepo.GetMostPopularBooks(5)
+	bookRepo := postgres.NewBooksRepository(dataBase.Connection)
+	books, _ := bookRepo.GetMostPopularBooks(5)
 	catRepo := postgres.CategoryRepoPq{}
 	cats, _ := catRepo.GetAllCategories()
 
