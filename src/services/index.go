67427780bb19e8c52d39a373f478806f13df0f6f
Den <away4people@gmail.com>
2018-06-05 20:01:06 +0300

    codefix

--- src/services/index.go
+++ src/services/index.go
@@ -5,39 +5,21 @@ import (
 	//"github.com/metalscreame/GoToBoox/src/models"
 	"net/http"
 	"github.com/metalscreame/GoToBoox/src/dataBase/postgres"
+	"github.com/metalscreame/GoToBoox/src/dataBase"
 	"github.com/metalscreame/GoToBoox/src/dataBase/repository"
 )
 
 func IndexHandler(c *gin.Context) {
-	type Categories struct{
-		ID int
-		Title string
-	}
-
-	type PopularBooks []struct{
-		Id int64
-		Title string
-		Description string
-		Popularity float64
-		CategoryId int64
-	}
-
 	type Data struct{
+		books []repository.Book
 		cats []repository.Categories
-		PopularBooks
 	}
 
-	categories, err := postgres.CategoryRepo.GetAllCategories()
-	if err != nil{
+	bookRepo := postgres.NewBooksRepository(dataBase.Connection)
+	books, _ := bookRepo.GetMostPopularBooks(5)
+	catRepo := postgres.CategoryRepoPq{}
+	cats, _ := catRepo.GetAllCategories()
 
-	}
-	popularBooks := PopularBooks{
-		{0, "Title-1", "Description", 5.0, 0},
-		{1, "Title-2", "Description", 4.5, 1},
-		{2, "Title-3", "Description", 4.2, 2},
-	}
-
-
-	output := Data{categories, popularBooks}
+	output := Data{books, cats}
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": output})
 }
