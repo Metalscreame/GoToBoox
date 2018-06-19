5a6a1f08d407be8cde2fc5fd55dce009de422e29
Roman <mantilocal@gmail.com>
2018-06-19 18:06:10 +0300

    updated sys envs

--- sys_variables.sh
+++ sys_variables.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-export POSTGRES_URL="postgres://niuaznefoznzqh:33f9db3d3723c0a337c18e6f0c599d358765159048ab0c4ec5a1d28969616854@ec2-54-217-214-68.eu-west-1.compute.amazonaws.com:5432/dcclgdtqr61bti"
-export PORT=8080
-echo PORT
+POSTGRES_URL='postgres://niuaznefoznzqh:33f9db3d3723c0a337c18e6f0c599d358765159048ab0c4ec5a1d28969616854@ec2-54-217-214-68.eu-west-1.compute.amazonaws.com:5432/dcclgdtqr61bti'
+PORT='8080'
