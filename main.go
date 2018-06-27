7f7126ee418fe19067e241e66adffb1283a7d510
Roman <mantilocal@gmail.com>
2018-06-27 10:33:10 +0300

    post merge vendor fix

--- main.go
+++ main.go
@@ -10,8 +10,6 @@ import (
 )
 
 func main() {
-	//os.Setenv("POSTGRES_URL","postgres://niuaznefoznzqh:33f9db3d3723c0a337c18e6f0c599d358765159048ab0c4ec5a1d28969616854@ec2-54-217-214-68.eu-west-1.compute.amazonaws.com:5432/dcclgdtqr61bti")
-	//os.Setenv("PORT","8080")
 	file:=setupLogFile()
 	defer file.Close()
 	dataBase.Connect()
