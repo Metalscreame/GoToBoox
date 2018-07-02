c7fd1515f85bd2df851902a31872bfc91dc92e1f
Den <away4people@gmail.com>
2018-07-02 19:48:34 +0300

    Update postgresBookRepo.go

--- src/dataBase/postgres/postgresBookRepo.go
+++ src/dataBase/postgres/postgresBookRepo.go
@@ -176,7 +176,7 @@ func (p booksRepositoryPG) GetByTagsAndRating(tags []string, rating []int) (book
 			"LEFT JOIN gotoboox.books_tags ON gotoboox.books.id = gotoboox.books_tags.id " +
 			"LEFT JOIN gotoboox.tags ON gotoboox.books_tags.tag_id = gotoboox.tags.tag_id " +
 			"WHERE gotoboox.tags.title = any($1) " +
-			"GROUP BY gotoboox.books.title, gotoboox.books.id " + 
+			"GROUP BY title, id " + 
 			"having count(*) = $2",
 			pq.Array(tags), tagsLen)
 		log.Print(rating)
